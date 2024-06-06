9bde78f6aac76781051b2a35bfc1aef538015bab
False N. Name <Mstow@protonmail.com>
2024-06-06 17:12:37 -0500

    Create struct and function signatures with lifetimes. Also run examples from rust book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+struct ImportantText<'a> {
+    text: &'a str,
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let string1 = String::from("abcd");
+        let string2 = String::from("Hello there!");
+        let result = longest(&string1, &string2);
+        println!("string2: {result}");
+    }
+    {
+        let novel = String::from("Call me Smart. Many moons ago...");
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        let i = ImportantText {
+            text: first_sentence,
+        };
+        println!("i: {:?}", i);
+        // Lifetime Elision up next
+    }
 }
